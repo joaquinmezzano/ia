43365f9c2e42316e0691f5b3d24706dad3e2f9da
Joaquin Mezzano <mezzanojoaquin@gmail.com>
2025-08-29 22:01:10 -0300

    Exercise 3

--- Algoritmos geneticos/Ejercicio 3/ej3.py
+++ Algoritmos geneticos/Ejercicio 3/ej3.py
@@ -7,17 +7,15 @@ creator.create("Individual", list, fitness=creator.FitnessMax)
 
 # Toolbox: atributos, individuos, población
 toolbox = base.Toolbox()
-toolbox.register("attr_bool", random.randint, 0, 1)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 6)
+toolbox.register("attr_int", random.randint, 0, 255)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, 3)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 # Función de evaluación
 def eval_maxones(individual):
-    result = 0
-    for index, value in enumerate(reversed(individual)):
-        if (value == 1):
-            result += index+1**2
-    return (-abs(result-num),)
+    r,g,b = individual
+    result = ((r-tripla[0])**2+(g-tripla[1])**2+(b-tripla[2])**2) ** 0.5
+    return (-result,)
 toolbox.register("evaluate", eval_maxones)
 
 # Operadores genéticos
@@ -26,8 +24,28 @@ toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 # Algoritmo principal
-num = int(input("Indique el número a comprobar: "))
 def main():
+    while True:
+        num1 = int(input("Indique el primer número (R)GB entre 0 y 255: "))
+        if (num1 >= 0 and num1 <= 255):
+            break
+        print("El número aceptable va de 0 a 255. Intente nuevamente.")
+
+    while True:
+        num2 = int(input("Indique el primer número R(G)B entre 0 y 255: "))
+        if (num2 >= 0 and num2 <= 255):
+            break
+        print("El número aceptable va de 0 a 255. Intente nuevamente.")
+
+    while True:
+        num3 = int(input("Indique el primer número RG(B) entre 0 y 255: "))
+        if (num3 >= 0 and num3 <= 255):
+            break
+        print("El número aceptable va de 0 a 255. Intente nuevamente.")
+
+    global tripla
+    tripla = (num1, num2, num3)
+
     random.seed(42)
     pop = toolbox.population(n=50)
     algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=20, verbose=True)
